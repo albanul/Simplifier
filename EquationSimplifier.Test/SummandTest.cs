05b7fd8cacb39a652c026de646629eb7bc493272
Alex Banul <albanul92@gmail.com>
2016-12-14 00:52:03 +0300

    Summand equality with tests

---
+++ EquationSimplifier.Test/SummandTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using EquationSimplifier.Entities;
+using Xunit;
+
+namespace EquationSimplifier.Test
+{
+	public class SummandTest
+	{
+		[Fact]
+		public void Equals_X1_X1_TrueReturned()
+		{
+			var x1 = new Variable("x", 1);
+
+			var firstVariables = new List<Variable> {x1};
+			var firstSummand = new Summand(2, firstVariables);
+
+			var seconndVariables = new List<Variable> {x1};
+			var secondSummand = new Summand(2, seconndVariables);
+
+			Assert.True(firstSummand == secondSummand);
+		}
+
+		[Fact]
+		public void Equals_X1Y2_X1Y2_TrueReturned()
+		{
+			var x1 = new Variable("x", 1);
+			var y2 = new Variable("y", 2);
+
+			var firstVariables = new List<Variable> { x1, y2 };
+			var firstSummand = new Summand(2, firstVariables);
+
+			var seconndVariables = new List<Variable> { x1, y2 };
+			var secondSummand = new Summand(2, seconndVariables);
+
+			Assert.True(firstSummand == secondSummand);
+		}
+
+		[Fact]
+		public void Equals_X1_X1Y2_FalseReturned()
+		{
+			var x1 = new Variable("x", 1);
+			var y2 = new Variable("y", 2);
+
+			var firstVariables = new List<Variable> { x1 };
+			var firstSummand = new Summand(2, firstVariables);
+
+			var seconndVariables = new List<Variable> { x1, y2 };
+			var secondSummand = new Summand(2, seconndVariables);
+
+			Assert.False(firstSummand == secondSummand);
+		}
+
+		[Fact]
+		public void Equals_X1Y2_Y2X1_TrueReturned()
+		{
+			var x1 = new Variable("x", 1);
+			var y2 = new Variable("y", 2);
+
+			var firstVariables = new List<Variable> { x1, y2 };
+			var firstSummand = new Summand(2, firstVariables);
+
+			var seconndVariables = new List<Variable> { y2, x1 };
+			var secondSummand = new Summand(2, seconndVariables);
+
+			Assert.True(firstSummand == secondSummand);
+		}
+	}
+}
--- EquationSimplifier/Entities/Summand.cs
+++ EquationSimplifier/Entities/Summand.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EquationSimplifier.Entities
 {
-	public struct Summand
+	public struct Summand : IEquatable<Summand>
 	{
 		public double Coeficient { get; set; }
 		public List<Variable> Variables { get; set; }
@@ -11,6 +13,37 @@ namespace EquationSimplifier.Entities
 		{
 			Coeficient = coeficient;
 			Variables = variables;
+		}
+
+		public static bool operator ==(Summand leftSummand, Summand rightSummand)
+		{
+			return leftSummand.Equals(rightSummand);
+		}
+
+		public static bool operator !=(Summand leftSummand, Summand rightSummand)
+		{
+			return !leftSummand.Equals(rightSummand);
+		}
+
+		public bool Equals(Summand other)
+		{
+			return Coeficient.Equals(other.Coeficient) &&
+					Variables.Count == other.Variables.Count &&
+					Variables.All(v => other.Variables.Contains(v));
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			return obj is Summand && Equals((Summand) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (Coeficient.GetHashCode() * 397) ^ (Variables?.GetHashCode() ?? 0);
+			}
 		}
 	}
 }
